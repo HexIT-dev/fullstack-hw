836fbe25267335a9ce1fe9a998006e44161f40fc
TokhirovIlhom <fotimasaidova3@gmail.com>
2026-04-01 09:12:41 +0500

    add root endpoint for Render

--- server.js
+++ server.js
@@ -6,18 +6,21 @@ import authRoutes from "./routes/authRoutes.js";
 import quoteRoutes from "./routes/quoteRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-
-
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// bu joyda authRoutes ulanishi shart
+// Routes
 app.use("/auth", authRoutes);
 app.use("/quotes", quoteRoutes);
 app.use("/users", userRoutes);
+
+// Root endpoint
+app.get("/", (req, res) => {
+  res.send("Backend API is running 🚀");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
